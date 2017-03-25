d6f231958134817aaa90cffff5e3a70736d0c7a8
bplindgren <bplindgren91@gmail.com>
2017-03-25 15:17:14 -0500

    salary info computed

--- salary.rb
+++ salary.rb
@@ -2,13 +2,29 @@ require 'csv'
 require 'pry'
 
 totalCost = 0
-numPlayers = 0
+numPlayers = 0.0
+totalWar = 0.00
 
-CSV.foreach('./datasets/master_updated.csv', headers: true, header_converters: :symbol) do |row|
+
+CSV.foreach('./public/datasets/players.csv', headers: true, header_converters: :symbol) do |row|
   totalCost += row[:salary].to_i
-  numPlayers+= 1
+  totalWar += row[:war].to_f
+  numPlayers+= 1.0
+
+  # if numPlayers % 100 == 0
+  #   p totalCost
+  #   p totalWar
+  # end
 end
 
-p totalCost
-p numPlayers
-p (totalCost/numPlayers)
+
+print "MLB players = ", numPlayers, "\n"
+p "============="
+print "total league pay = $", totalCost, "\n"
+print "average salary = $", (totalCost/numPlayers).round(0), "\n"
+p "============="
+print "total league WAR = ", totalWar.round(1), "\n"
+print "average WAR = ", (totalWar/numPlayers).round(3), "\n"
+p "============="
+print "average cost per win = $", (((totalCost/numPlayers).round(0))/(totalWar/numPlayers).round(3)).round(0), "\n"
+
